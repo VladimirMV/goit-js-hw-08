058a45de7051e3227e77433876209b2ff169ea95
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-01-29 21:15:45 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
 console.log(galleryItems);
+
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+console.log(galleryContainer);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+         
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}"   alt = "${description}" /> </a>
+        `;
+    })
+    .join("");
+    };
+
+
+    
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250,
+  captionsData: 'alt',
+})
+
+ 
