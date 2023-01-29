7d9fbcc8985bbc68469d3e8817e47d4070a1e5e7
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-01-30 00:24:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
 
+import   throttle  from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-msg';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 200));
